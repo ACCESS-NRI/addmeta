2077d51697dfc0a1bc6dfcb4f90f08d24036b044
Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
2025-10-09 10:28:17 +1100

    Added full run-through example test

---
+++ test/examples/ocean/setup.sh
@@ -0,0 +1,4 @@
+ncgen -7 -o test.nc ../../test/test.cdl
+cp test.nc ocean-2d-wind_power_u-1monthly-mean-ym_0792_01.nc
+cp test.nc ocean-3d-power_diss_drag-1yearly-mean-ym_0792_07.nc
+cp test.nc oceanbgc-3d-zprod_gross-1monthly-mean-ym_0792_01.nc
